299ac70a2d6707fa39f87fce2f9150dc98976499
Holayn <kwwong1597@gmail.com>
2022-02-26 01:13:10 -0500

    prepare for deployment

--- packages/server/index.js
+++ packages/server/index.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/', express.static(path.join(__dirname, '../web/dist')));
 app.use('/api', routes);
 app.use('/favicon.ico', express.static(path.join(__dirname, './static/favicon.ico')));
 
